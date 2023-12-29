fd6b55a30100db028e3bbb06dafbef7139c8ce83
Luccadz75 <149024268+Luccadz75@users.noreply.github.com>
2023-12-29 05:43:16 +0000

    commit

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extensions = ['.com', '.net', '.us', '.io'];
+
+for (let p of pronoun) {
+    for (let a of adj) {
+        for (let n of noun) {
+            for (let ext of extensions) {
+                console.log(`${p}${a}${n}${ext}`);
+            }
+        }
+    }
+}
